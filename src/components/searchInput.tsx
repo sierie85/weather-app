c97d03a997e154dc6fbb4771c8b36b3f308d3dec
sbrieschenk <s.brieschenk@gmail.com>
2024-03-14 20:30:13 +0100

    search-adjust

--- src/components/searchInput.tsx
+++ src/components/searchInput.tsx
@@ -33,14 +33,14 @@ export default function SearchInput({
           onChange={(e) => setSQuery(e.currentTarget.value)}
           type="search"
           id="loc-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 bg-white focus:ring-slate-500 focus:border-slate-600 !outline-none"
+          className="block w-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 bg-white focus:ring-slate-500 focus:border-slate-600 !outline-none"
           placeholder="Berlin, Amsterdam, Paris ..."
           required
           autoFocus
         />
         <button
           type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-slate-500 hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300 text-sm px-4 py-2"
+          className="hidden md:block text-white absolute end-2.5 bottom-2.5 bg-slate-500 hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300 text-sm px-4 py-2"
         >
           Search
         </button>
